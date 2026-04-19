e37e9b1136bc6c7f45933aef905c9d0cb087f9ed
KerbalMissile <kerbalmissile@gmail.com>
2026-04-19 11:08:33 -0600

    added proper comments

--- Kodo/Source/App.axaml.cs
+++ Kodo/Source/App.axaml.cs
@@ -1,9 +1,12 @@
+// Licensed under the Kodo Public License v1.0
+// April 19th, 2026 - KerbalMissile - proper comments added
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 namespace Kodo;
 
+// The main application class for Kodo, responsible for initializing the application and setting up the main window.
 public partial class App : Application
 {
     public override void Initialize()
