8f5ffcfebd5632be968977ce45baee8e712e58b3
SS-YYC <shreyas.yyc@gmail.com>
2026-07-26 13:19:56 -0600

    Create KEYS.cs with placeholder Aptabase key
    Added a placeholder KEYS.cs file with an Aptabase key.

---
+++ Kodo/Source/KEYS.cs
@@ -0,0 +1,9 @@
+// Licensed under GPL-v3.0
+
+// Placeholder KEYS.cs - allows a build without compromising the Aptabase key
+namespace Kodo;
+
+internal static class KEYS
+{
+    public const string AptabaseKey = "EXAMPLE";
+}
