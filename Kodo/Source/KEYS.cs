faa2afb2634558bf06e87f0dfef9249a4d92121a
SS-YYC <shreyas.yyc@gmail.com>
2026-07-28 10:36:41 -0600

    Add files via upload

--- Kodo/Source/KEYS.cs
+++ Kodo/Source/KEYS.cs
@@ -1,6 +1,6 @@
 // Licensed under GPL-v3.0
 
-// Placeholder KEYS.cs - allows you to build Kodo without exposing the Aptabase key
+// Placeholder KEYS.cs - allows a build without compromising the Aptabase key
 namespace Kodo;
 
 internal static class KEYS
